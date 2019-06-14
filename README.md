0ca6dc26e2260754bf12adc2744fc9954d325165
nelson.marcos <nelson.marcos@corp.globo.com>
2019-06-14 13:44:37 -0300

    Install and README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # docker-alias
+
+## Install
+
+### Wild mode
+```
+curl -o ~/.docker-aliases.sh https://raw.githubusercontent.com/nelsonmarcos/docker-aliases/master/docker-aliases.sh 
+echo "source ~/.docker-aliases.sh" >> ~/.bash_profile
+```
+Open a new shell and have fun!
+
+### Controlled mode
+```
+git clone https://github.com/nelsonmarcos/docker-aliases.git
+cd docker-aliases
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 function log {
         echo [$(date "+%d/%m/%Y %H:%M:%S")] - $@
 }
-script="~/.docker-aliases.sh"
+script="docker-aliases.sh"
 
-if [ -f $script ];then
+if [ -f ~/.$script ];then
   log "Backing up current $script"
-	log $(cp -v $script $script.$(date +%Y%m%d%S))
+	log $(cp -v ~/.$script ~/.$script.$(date +%Y%m%d%S))
 fi
 log  "Copying $script to your home dir"
-log $(cp -v .docker-alias.sh ~/)
-log Finished
+log $(cp -v $script ~/.$script)
+grep -q "source ~/.${script}" ~/.bashrc || grep -q "source ~/.${script}" ~/.bash_profile
+if [ $? -ne "0" ];then
+	log "Adding source ~/.${script} to ~/.bash_profile"
+	echo "source ~/.${script}" >> ~/.bash_profile
+fi
+log "Success"
