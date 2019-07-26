e284436b04415c72281ae617f4637018306f057a
nelson.marcos <nelson.marcos@corp.globo.com>
2019-07-26 15:37:07 -0300

    Fix docker-aliases.sh

--- docker-aliases.sh
+++ docker-aliases.sh
@@ -1,1 +1,1 @@
-alias docker-rm-all="docker container rm -f $(docker container ls -a|grep -v CONTAINER|awk '{print $1}')"
+alias docker-rm-all="docker container rm -f \$(docker container ls -a|grep -v CONTAINER|awk '{print \$1}')"
